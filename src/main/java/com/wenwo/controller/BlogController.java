d94e1f7992355c3d69df0fcbe1a81d698299c793
zwq <2298445766@qq.com>
2022-03-07 22:44:23 +0800

    集成Swagger

--- src/main/java/com/wenwo/controller/BlogController.java
+++ src/main/java/com/wenwo/controller/BlogController.java
@@ -3,10 +3,14 @@ package com.wenwo.controller;
 import com.github.pagehelper.Page;
 import com.wenwo.entity.Blog;
 import com.wenwo.service.BlogService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+@Api(tags = "springboot学习工程相关接口")
 @RestController
 public class BlogController {
     @Autowired
@@ -21,6 +25,10 @@ public class BlogController {
         //return null;
     }
 
+    @ApiOperation("根据id查询日志接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "日志id")
+    })
     @GetMapping(value = "/blog/{id}")
     public Blog getBlogById(@PathVariable Integer id) {
         System.out.println("*****************"+id);
