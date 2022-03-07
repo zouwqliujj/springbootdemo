65991299815c912b474bac25115eb604c8d495a2
zwq <2298445766@qq.com>
2022-03-07 22:14:09 +0800

    修改redis缓存配置

--- src/main/java/com/wenwo/SpringbootdemoApplication.java
+++ src/main/java/com/wenwo/SpringbootdemoApplication.java
@@ -3,11 +3,9 @@ package com.wenwo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.wenwo.mapper")
-@EnableCaching
 public class SpringbootdemoApplication {
 
     public static void main(String[] args) {
